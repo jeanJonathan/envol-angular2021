034b31243839c5b0d3b3cd70b37db6ff4b7a7c96
jean-jonathan <jean-jonathan.koffi@etud.univ-pau.fr>
2023-02-14 14:44:13 +0100

    Manipulation de la propriete de bouton par la liaison a une variable du Component racine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,13 @@ export class AppComponent {
   title = 'Bienvenue dans le TP Angular ENVOL 2021';
   superheroes : string[] = ["Hulk","Spider-Man","Thor","Iron Man"];
   newHero: string = 'Black Widow';
-  newHeroAdded: boolean = false;
+  newHeroAdded: boolean = false; // la proprite de siable du bouton reste true alors
 
   addNewHero():void{ //pas obliger de preciser le type de retour void
-      if (!this.superheroes.includes(this.newHero)){
-        this.superheroes.push(this.newHero);
-        this.newHeroAdded = true;
+      if (this.superheroes.includes(this.newHero)){
+          this.newHeroAdded = false; // le bouton reste activer (la propriete de disable reste false) car la propriete est desactiver
       }
+    this.superheroes.push(this.newHero);
+    this.newHeroAdded = true; // le bouton se desactive (la propriete de disable devient true) car la propriete activee
   }
 }
